217b7e171525f209febc2c55be517f4cb630c7a8
you <anusorn1039@gmail.com>
2023-02-20 15:16:24 +0700

    Output and Input

--- c_basic.cpp
+++ c_basic.cpp
@@ -1,9 +1,12 @@
 #include <stdio.h>
-int a = 10 , b ;
+char a ;
 int functionX(int,int );
 int main ()
 {
-	printf("%d",functionX(10,10));  
+	printf("Enter value number :");
+	scanf("%d",&a);  
+	printf("Display value number a : %c",a);
+	printf("Square value %c",functionX(a,a));
 	return 0 ;
 }
 int functionX(int aa,int bb)
